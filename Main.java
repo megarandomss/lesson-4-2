ff25c5dd43bd75afa786ac5b7913724164b09497
Makovey Andrey <megarandom@mail.ru>
2022-02-28 15:49:53 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Ivan", "Petrovich", "Sidorov", 35);
+        person1.printName();
+
+//        Person person2 = new Person("Irina", "Pavlovna", "Sokolova");
+//        person1.printName();
+
+//        person1.setName("Pavel");
+//        person1.printName();
+        person1.printAge();
+    }
+}
---
+++ Person.java
@@ -0,0 +1,57 @@
+public class Person {
+
+    private String name;
+    private String middleName;
+    private String familyName;
+    private int age;
+
+
+    public Person(String name, String middleName, String familyName, int age) {
+        this.familyName = familyName;
+        this.name = name;
+        this.middleName = middleName;
+        this.age = age;
+    }
+
+    public void printName() {
+        System.out.println("Имя: " + name);
+    }
+
+
+    public void setName(String name) {
+        if (name.length() >= 2){
+        this.name = name;}
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void printAge() {
+        System.out.println("Возраст: " + age);
+    }
+}
